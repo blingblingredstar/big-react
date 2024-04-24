8a8e6a4d6afcb86ead6fbc7d91760d99cd4175c4
yong.liu <564447023@qq.com>
2024-04-24 21:27:40 +0800

    Add Prettier and ESLint configurations

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import('prettier').Options}
+ */
+const config = {
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+};
+
+export default config;
---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+const config = [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
+
+export default config;
